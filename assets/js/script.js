76af7f514b00fe6434eedf410734470fe9ea0bb7
Elizabeth Kelmenson <esk99@cornell.edu>
2024-04-15 01:28:35 -0400

    Starter website structure

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript code to handle interactions on your site
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Page loaded');
+  // Add more JavaScript code here
+});
